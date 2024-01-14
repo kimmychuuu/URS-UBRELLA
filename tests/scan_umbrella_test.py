0a46ec44305b8723d59951c843ca92d93ae343d3
kimmychuuu <kmgllns25@gmail.com>
2024-01-14 18:03:18 +0800

    modified:   gui.py new file:   tests/date_test.py modified:   tests/scan_umbrella_test.py

--- tests/scan_umbrella_test.py
+++ tests/scan_umbrella_test.py
@@ -1,19 +1,16 @@
 import sys
 sys.path.append('../')
 
-import urs_umbrella
-import os
+from urs_umbrella
+import Machine
 
-from dotenv import load_dotenv
-
-load_dotenv()
-arduino_port = os.getenv('arduino_port')
-sim808_port = os.getenv('sim808_port')
-api_endpoint = os.getenv('api_url')
-api_key = os.getenv('api_key')
-
-machine = urs_umbrella.Machine(arduino_port, sim808_port, api_endpoint, api_key, hardware_callbacks='thread')
-
+machine = Machine(
+        arduino_port='/dev/ttyUSB1',
+        sim808_port='/dev/ttyUSB0',
+        api_key='URSUmbrella@2023',
+        api_url='https://ursubrella.online/api',
+        hardware_callbacks='thread'
+    )
 umbrella_id = machine.scan_qrcode()
 
 machine.confirm_umbrella('1','23URS01')
